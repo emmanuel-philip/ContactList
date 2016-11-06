4b13c70831fdecb7a4aba6966ae8258e4069a551
emmanuel-philip <emmanuelrajuphilip@gmail.com>
2016-11-06 18:01:57 +0530

    vcs.xml

---
+++ ContactList/app/src/main/java/com/example/emman/contactlist/DividerItemDecoration.java
@@ -0,0 +1,10 @@
+package com.example.emman.contactlist;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by emman on 11/6/2016.
+ */
+
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+}
